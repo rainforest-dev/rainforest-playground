fb107d08f7f0cd6f22474a9da931fb822530f3e0
r08521610 <r08521610@ntu.edu.tw>
2022-08-14 00:18:48 +0800

    3 files modified, 2 deleted

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -48,16 +48,47 @@
       <label for="solid">Solid</label>
     </div>
   </div>
-  <div :style="styleObj" class="container">
+  <div :style="{ ...styleObj, backgroundColor: '#0B2127' }" class="container">
     <img :src="star" alt="star" class="star" />
     <div class="card" v-show="index === 'solid'"></div>
+  </div>
+  <div
+    class="bg"
+    :style="{
+      position: 'relative',
+      background: '#0b2127',
+    }"
+  >
+    <div class="gooey-effect" ref="gooeyEffectRef">
+      <div class="circle circle1"></div>
+      <div class="circle circle2" :style="mousePosStyle">
+        {{ mousePosStyle }}
+      </div>
+    </div>
+    <!-- <div class="blur"></div> -->
+    <div :style="styleObj" class="container2"></div>
+    <svg>
+      <filter id="gooey">
+        <feGaussianBlur in="SourceGraphic" stdDeviation="10" />
+        <feColorMatrix
+          values="
+        1 0 0 0 0
+        0 1 0 0 0
+        0 0 1 0 0
+        0 0 0 20 -10
+      "
+        />
+      </filter>
+    </svg>
   </div>
 </template>
 <script setup>
 import { ref, computed } from 'vue';
 import svgToMiniDataURI from 'mini-svg-data-uri';
-import url from '../assets/bg.png';
-import star from '../assets/star.png';
+import url from '../assets/bg.svg';
+import star from '../assets/star.svg';
+import { useMouse } from '@vueuse/core';
+const { x, y } = useMouse();
 const index = ref('solid');
 const type = ref('fractalNoise');
 const baseFrequency = ref(0.5);
@@ -83,12 +114,20 @@ const styleObj = computed(() => {
     case 'solid':
       return {
         background: `url(${url}), url("${svg.value}")`,
-        backgroundColor: '#0B2127',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         height: '50vh',
       };
   }
+});
+const gooeyEffectRef = ref(null);
+const mousePosStyle = computed(() => {
+  const rect = gooeyEffectRef.value?.getBoundingClientRect();
+  if (!rect) return {};
+  return {
+    top: `${y.value - rect.top - rect.height / 2}px`,
+    left: `${x.value - rect.left - rect.width / 2}px`,
+  };
 });
 </script>
 <style>
@@ -120,5 +159,62 @@ const styleObj = computed(() => {
   position: absolute;
   right: 0;
   bottom: 0;
+}
+.gooey-effect {
+  height: 300px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  filter: url(#gooey);
+}
+.blur {
+  position: absolute;
+  right: 0;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  backdrop-filter: blur(20px);
+  background-color: #ffffff30;
+}
+.container2 {
+  position: absolute;
+  right: 0;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  backdrop-filter: blur(20px);
+}
+.circle {
+  position: relative;
+  width: 100px;
+  height: 100px;
+  background-color: #fff;
+  border-radius: 50%;
+  pointer-events: none;
+}
+.circle:before {
+  content: '';
+  position: absolute;
+  top: -50px;
+  bottom: -50px;
+  left: -50px;
+  right: -50px;
+  border-radius: 50%;
+  filter: blur(50px);
+  background-color: inherit;
+  z-index: -1;
+}
+.circle1 {
+  background: linear-gradient(90deg, #abe661, #abe661, #29cbb4);
+}
+.circle2 {
+  width: 200px;
+  height: 200px;
+  background: linear-gradient(90deg, #29cbb4, #abe661, #abe661);
+}
+svg {
+  height: 0;
+  width: 0;
 }
 </style>
