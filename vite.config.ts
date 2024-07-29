487ccc27fceb3a2552e40adb881a453512e56c00
Rainforest Cheng <rainforestnick@gmail.com>
2024-07-29 03:08:25 +0000

    refactor: use nx

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import pages from 'vite-plugin-pages'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), pages()]
-})
